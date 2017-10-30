0033c4eb4cd713e15b3f55ad5e997e083767fde2
Michael Shinners <mishinners@icloud.com>
2017-10-30 08:42:32 -0700

    tests actually working now.

--- test_sum_of_the_first_nth_term.py
+++ test_sum_of_the_first_nth_term.py
@@ -10,22 +10,19 @@ import pytest
 # Test.assert_equals(series_sum(3), "1.39")
 
 
-def test_series_sum(n):
+def test_series_sum():
     """Test that function finds the right number."""
     from sum_of_the_first import series_sum
-    s = series_sum()
-    assert s(1) == "1.00"
+    assert series_sum(1) == "1.00"
 
 
-def test_series_sum_2(n):
+def test_series_sum_2():
     """Test that function finds the right number."""
     from sum_of_the_first import series_sum
-    s = series_sum()
-    assert s(24) == "2.10"
+    assert series_sum(24) == "2.10"
 
 
-def test_series_sum_3(n):
+def test_series_sum_3():
     """Test that function finds the right number."""
     from sum_of_the_first import series_sum
-    s = series_sum()
-    assert s(42) == "2.29"
+    assert series_sum(42) == "2.29"
